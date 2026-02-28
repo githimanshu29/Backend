b745a99e5a388794f2f59bfdc261f608d25c5b30
githimanshu29 <himanshusagarofficial420@gmail.com>
2026-03-01 03:58:18 +0530

    api gateway and rate-limiters applied

--- services/api-gateway/src/server.js
+++ services/api-gateway/src/server.js
@@ -1,16 +1,72 @@
 import express from "express";
 import dotenv from "dotenv";
 import { createProxyMiddleware } from "http-proxy-middleware";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import morgan from "morgan";
+
 
 dotenv.config();
 
 const app = express();
+app.disable("x-powered-by");
+
+app.use(
+  helmet({
+    contentSecurityPolicy:false,
+    crossOriginEmbedderPolicy:false,
+  })
+);
+
+
+
+app.use(morgan("combined"));
+
+//Login limiter
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    message: "Too many login attempts. Try again later."
+  }
+});
+
+//Register Limiter
+const registerLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 10,
+  message: {
+    success: false,
+    message: "Too many accounts created from this IP."
+  }
+});
+
+//general API limiter
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: {
+    success: false,
+    message: "Too many requests. Please slow down."
+  }
+});
+
+
+//  Apply rate limiters
+app.use("/api/auth/login", loginLimiter);
+app.use("/api/auth/register", registerLimiter);
+app.use("/api", apiLimiter);
 
 /*
 =========================================
 AUTH SERVICE PROXY
 =========================================
 */
+
+
 
 app.use((req, res, next) => {
     console.log("Incoming request:", req.method, req.url);
@@ -53,6 +109,16 @@ app.get("/health", (req, res) => {
 });
 
 const PORT = 5000;
+
+
+app.use((err, req, res, next) => {
+  console.error("Gateway Error:", err);
+
+  res.status(500).json({
+    success: false,
+    message: "Gateway Internal Error"
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`🚀 API Gateway running on port ${PORT}`);
